9e8135d1cfac630661d9c10898d5916f25bdade0
yaohuangguan <yaob@miamioh.edu>
2020-06-12 16:32:12 +0800

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const EventEmitter = {
+    events: {},
+    dispatch(event, data) {
+        if (!this.events[event]) return;
+        this.events[event].forEach(callback => callback(data));
+    },
+    subscribe(event, callback) {
+        if (!this.events[event]) this.events[event] = [];
+        this.events[event].push(callback);
+    },
+    off(event) {
+        delete this.events[event];
+    },
+};
+
+
+export default EventEmitter;
